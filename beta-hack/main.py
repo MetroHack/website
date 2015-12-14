7310d5f3fe96242a6e2ed8762d9e116c45e05bc0
Luis Eduardo Bello <luis.bello.mila@gmail.com>
2015-12-14 15:58:49 -0500

    Arreglando código

--- beta-hack/main.py
+++ beta-hack/main.py
@@ -65,7 +65,7 @@ class Home(MainHandler):
 class Admin(MainHandler):
 
 	def render_home(self,title=""):
-			self.render("index.html")
+			self.render("loginAdmin.html")
 			
 	def get(self):
     		self.render_home()
