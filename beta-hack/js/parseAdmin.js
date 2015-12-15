9245a553ff5c81a53823dd25158d07654a329c06
marcelojosel15 <marcelojosel15@gmail.com>
2015-12-15 16:56:12 -0430

    Listo el login

--- beta-hack/js/parseAdmin.js
+++ beta-hack/js/parseAdmin.js
@@ -3,3 +3,25 @@ $(function() {
   //Inicializar Parse
     Parse.initialize("pVQEqn1cuqxLP8BL0LSbkB18p9Tii4wWi9EMfuzv", "6cceoL2U1WqxgUUls7dFj7CHwyEZeN47vTxySufM");
 });
+ 
+
+//Funcion para hacer login
+function adminLogin (form) {
+    
+    //Creamos las variables del texto introducido por el usuario
+    var user =form.user.value;
+    var password = form.password.value;
+    
+    //Metodo de Parse para login
+    Parse.User.logIn(user, password, {
+          success: function(user) {
+          // Do stuff after successful login.
+              alert("Bienvenido");
+          //Aqui va el codigo para pasar a la siguiente pagina de admin
+          },
+          error: function(user, error) {
+          // The login failed. Check error to see why.
+              alert("Clave o Usuario Incorrecto");
+          }
+    });
+}
