42fe5aeca466519baec0089700886b2d1721e4eb
marcelojosel15 <marcelojosel15@gmail.com>
2015-12-19 18:12:30 -0430

    Empty

--- beta-hack/js/parseCreateEvent.js
+++ beta-hack/js/parseCreateEvent.js
@@ -2,14 +2,18 @@ $(function() {
     Parse.$ = jQuery;
   //Inicializar Parse
     Parse.initialize("pVQEqn1cuqxLP8BL0LSbkB18p9Tii4wWi9EMfuzv", "6cceoL2U1WqxgUUls7dFj7CHwyEZeN47vTxySufM");
+    checkLogin();
+});
+
+
+function checkLogin(){
     if (Parse.User.current()){
      //alert("Usuario Comprobado");   
     }else{
      alert("No eres administrador");
      location.replace("http://beta-hack.appspot.com");
-    }
-});
-
+    } 
+}
 
 
 function crearEvento (form) {
