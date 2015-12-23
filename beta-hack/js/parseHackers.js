d73af4829251fb6af4cb920ba02c53b8e0d6ad3e
marcelojosel15 <marcelojosel15@gmail.com>
2015-12-23 18:34:17 -0430

    Empty

--- beta-hack/js/parseHackers.js
+++ beta-hack/js/parseHackers.js
@@ -2,7 +2,7 @@ $(function() {
     Parse.$ = jQuery;
   //Inicializar Parse
     Parse.initialize("pVQEqn1cuqxLP8BL0LSbkB18p9Tii4wWi9EMfuzv", "6cceoL2U1WqxgUUls7dFj7CHwyEZeN47vTxySufM");
-   // checkLogin();
+    checkLogin();
     getUsuarios();
 });
 
