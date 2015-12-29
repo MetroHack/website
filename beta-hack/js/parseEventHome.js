281b28e9ebbd830c57ad794aee9a36004d1feb88
Luis Eduardo Bello <luis.bello.mila@gmail.com>
2015-12-29 12:13:42 -0500

    Codigo de eventos home listo

---
+++ beta-hack/js/parseEventHome.js
@@ -0,0 +1,51 @@
+$(function() {
+    Parse.$ = jQuery;
+  //Inicializar Parse
+    Parse.initialize("pVQEqn1cuqxLP8BL0LSbkB18p9Tii4wWi9EMfuzv", "6cceoL2U1WqxgUUls7dFj7CHwyEZeN47vTxySufM");
+    getEvent();
+});
+
+
+
+function getEvent(){
+    var Evento = Parse.Object.extend("Event");
+    var query = new Parse.Query(Evento);
+    query.find({
+          success: function(results) {
+                //alert("Successfully retrieved " + results.length + " scores.");
+                // Do something with the returned Parse.Object values
+                for (var i = 0; i < results.length; i++) {
+                  var object = results[i];
+
+                  var titulo = object.get('titulo');
+                  var fecha = object.get('fecha');
+                  var lugar = object.get('lugar');
+                  var imagen = object.get('imagenUrl');
+                  var registro = object.get('registro');
+                  var descripcion = object.get('descripcion');
+                  
+                  output += "<li>";
+                  output += "<ul class=\"cd-item-wrapper\">";
+                  output += "<li class=\"cd-item-front\"><a href=\"#0\"><img src=\""+imagen+"\" alt=\"Preview image\"></a></li>";
+                  output += "</ul>";
+                  output += "<div class=\"cd-item-info\">";
+                  output += "<b><a href=\"#0\">"+titulo+"</a></b>";
+                  output += "</div>";
+                  output += "</li>";
+                  
+                }	
+                          alert("Titulo: "+titulo);	
+                  //LLenamos el HTML	                  
+                  $("#eventos").html(output);
+          },
+          error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+          }
+          
+          
+    });    
+    
+    	
+}
+
+
