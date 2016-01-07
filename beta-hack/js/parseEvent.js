c2bcee6e566950c97c243055add2205d3da065a9
marcelojosel15 <marcelojosel15@gmail.com>
2016-01-07 14:53:51 -0430

    Listo el posho

--- beta-hack/js/parseEvent.js
+++ beta-hack/js/parseEvent.js
@@ -5,6 +5,9 @@ $(function() {
     //***************************************ESTEBAN Y LUIS***************************************
     //AQUI YO LLAME LA FUNCION PARA QUE SE EJECUTE DE UNA, PERO LLAMENLA CUANDO SEA NECESARIO PARA USTEDES
     getEvent();
+    comprobarRegistro();
+    //Parse.User.logOut(); 
+    //logIn();
 });
 
 
@@ -20,7 +23,6 @@ function getEvent(){
                 // Do something with the returned Parse.Object values
                 for (var i = 0; i < results.length; i++) {
                   var object = results[i];
-
                   var titulo = object.get('titulo');
                   var fecha = object.get('fecha');
                   var lugar = object.get('lugar');
@@ -69,6 +71,129 @@ function getEvent(){
     });    
     
     	
+}
+
+
+  //Comprobamos que el usuario esta registrado en MetroHack
+  function comprobarUsuario(){
+      //Usuario Registrado
+      if (Parse.User.current()){
+          //alert("ESTA FIUNCIONANDO");
+          return true;
+          
+      //Usuario no registrado
+      }else{
+          //alert("aja");
+          return false;
+      }  
+  }
+
+
+
+//En esta funcion comprobamos si el usuario ya esta registrado Luis
+function comprobarRegistro(){
+    
+            //Si el usuario si esta registrado en MetroHack, te registra al evento con tu user.
+            if (comprobarUsuario()){
+                
+                        //Creamos las variables
+                        var user = Parse.User.current(); 
+                        var id = window.location.hash.substr(1);
+                        var Registro = Parse.Object.extend("RegistradoEvento");
+                
+                        //Hacemos el Query
+                        var query = new Parse.Query(Registro);
+                        query.equalTo("usuario", user);
+                        query.equalTo("eventoID",id);
+                        query.find({
+                            
+                                    success: function(results) {
+                                    //*********************************************
+                                    //Aqui va el codigo para bloquear el boton LUIS
+                                        
+                                        
+                                        
+                                    },
+                            
+                            
+                            
+                                      error: function(error) {
+                                            alert("Error: " + error.code + " " + error.message);
+                                      }
+          
+          
+                          });     
+ 
+            }
+    
+}
+
+//***************************************************************************************************************************************
+
+//Funcion para registrar al usuario en el evento
+function registrarUsuario(){
+    
+            //Si el usuario no esta registrado en MetroHack, te manda al home para que lo hagas.
+            if (comprobarUsuario() == false){
+
+                        alert("No estas registrado en MetroHack, vuelve cuando ya estes registrado");
+                        location.replace("http://metro-hack.appspot.com");
+
+                
+            //Si el usuario si esta registrado en MetroHack, te registra al evento con tu user. 
+            }else{
+                       
+
+                        //Creamos las variables
+                        var user = Parse.User.current(); 
+                        var id = window.location.hash.substr(1);
+                        var Registro = Parse.Object.extend("RegistradoEvento");
+                        var registro = new Registro();
+                        var Evento = Parse.Object.extend("Event");
+                
+                        //Buscamos el evento que se quiere registrar
+                        var query = new Parse.Query(Evento);
+                        query.equalTo("objectId", id);
+                        query.find({
+                            
+                                    success: function(results) {
+                                            //Aqui ya se registra al usuario
+                                            registro.set("usuario",user);
+                                            registro.set("evento",results[0]);
+                                            registro.set("eventoID",id);
+                                            registro.save();
+                                            alert("Registrado");
+                                        
+                                        
+                                    },
+                            
+                            
+                            
+                                      error: function(error) {
+                                            alert("Error: " + error.code + " " + error.message);
+                                      }
+          
+          
+                          });    
+    
+                    
+            }
+    
+}
+
+
+
+function logIn(){
+Parse.User.logIn("marcelo", "20131110015", {
+  success: function(user) {
+    //alert(Parse.User.current().id);
+    // Do stuff after successful login.
+  },
+  error: function(user, error) {
+    // The login failed. Check error to see why.
+  }
+});
+    
 }
 
 
